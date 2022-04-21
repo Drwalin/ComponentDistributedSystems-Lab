b7d3901767742286f81610ce9a1b0761cd847927
Drwalin <garekmarek@wp.pl>
2022-04-21 19:01:52 +0200

    RabbitMQ

---
+++ lab7-rabbitmq/Client-dotnetframework/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using RabbitMQ.Client;
+using System.Diagnostics;
+using RabbitMQ.Client.Events;
+
+namespace Client_dotnetframework {
+	class Program {
+		static void Main(string[] args) {
+			var factory = new ConnectionFactory() {
+				UserName = "mapaayxd",
+				Password = "HmuXDb9Jj-crT9SGJ2XCRV2yTqQZg5EU",
+				HostName = "sparrow.rmq.cloudamqp.com",
+				VirtualHost = "mapaayxd"
+			};
+
+
+			var pid = Process.GetCurrentProcess().Id;
+			using(var connection = factory.CreateConnection())
+			using(var channel = connection.CreateModel()) {
+				IBasicProperties prop = channel.CreateBasicProperties();
+				prop.Headers = new Dictionary<string, object>();
+				prop.Headers.Add("test", 311);
+				prop.Headers.Add("mleko", 10);
+
+				channel.QueueDeclare("message_queue", false, false, false, null);
+				channel.QueueDeclare("response", false, false, false, null);
+
+
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) => {
+					var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+					Console.WriteLine(pid + " odbiera odpowiedx: " + message);
+					// show message
+				};
+				channel.BasicConsume("response", true, consumer);
+
+				for(int i=0; i<10; ++i) {
+					var msg = pid + " wysyla wiadomosc " + i;
+					var bytes = Encoding.UTF8.GetBytes(pid + " wysyla wiadomosc " + i);
+					Console.WriteLine(msg);
+					channel.BasicPublish("", "message_queue", prop, bytes);
+					Thread.Sleep(1000);
+				}
+
+
+				Console.ReadKey();
+			}
+		}
+	}
+}
---
+++ lab7-rabbitmq/Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using RabbitMQ.Client;
+using System.Diagnostics;
+using RabbitMQ.Client.Events;
+
+namespace Client_dotnetframework {
+	class Program {
+		static void Main(string[] args) {
+			var factory = new ConnectionFactory() {
+				UserName = "mapaayxd",
+				Password = "HmuXDb9Jj-crT9SGJ2XCRV2yTqQZg5EU",
+				HostName = "sparrow.rmq.cloudamqp.com",
+				VirtualHost = "mapaayxd"
+			};
+			var pid = Process.GetCurrentProcess().Id;
+			using(var connection = factory.CreateConnection())
+			using(var channel = connection.CreateModel()) {
+				channel.QueueDeclare("message_queue", false, false, false, null);
+				channel.QueueDeclare("response", false, false, false, null);
+				
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) => {
+					var h = ea.BasicProperties.Headers;
+					var body = ea.Body;
+					var message = Encoding.UTF8.GetString(body.ToArray());
+
+					Console.WriteLine(pid + " odbiera: " + message + "   ``` headers: test: " + h["test"] + ", mleko: " + h["mleko"]);
+					Thread.Sleep(2000);
+					Console.WriteLine(pid + " odbiera: " + message + "   ``` wykonane");
+
+					var msg = pid + " odpowiedz na: " + message;
+					var bytes = Encoding.UTF8.GetBytes(msg);
+					Console.WriteLine(msg);
+					channel.BasicPublish("", "response", null, bytes);
+				};
+				consumer.Model.BasicQos(0, 1, false);
+				channel.BasicConsume("message_queue", true, consumer);
+
+				Console.ReadKey();
+			}
+		}
+	}
+}
